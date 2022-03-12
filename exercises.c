d46e39bef6106db3d47f674c1197a93c2f04e074
Your Name <you@example.com>
2022-03-12 16:25:41 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -73,7 +73,7 @@ typedef struct {
 
 Vector * crearVector(int n) {
    Vector *nuevoVector = NULL;
-   *nuevoVector->datos=NULL;
+   nuevoVector->datos=NULL;
    nuevoVector->datos = (int*) calloc(n,sizeof(int));
    if(nuevoVector->datos == NULL)
    {
